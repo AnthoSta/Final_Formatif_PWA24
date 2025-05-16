9b5cd977357b29b843897c644dde5287f9853a3f
AnthoSt <2390005@cegepmontpetit.ca>
2025-05-16 11:00:02 -0400

    Fin exercice animation

--- ngAnimations/src/app/app.component.ts
+++ ngAnimations/src/app/app.component.ts
@@ -1,16 +1,52 @@
 import { transition, trigger, useAnimation } from '@angular/animations';
 import { Component } from '@angular/core';
-import { bounce, shakeX, tada } from 'ng-animate';
+import { bounce, flash, shake, shakeX, tada } from 'ng-animate';
+import { delay } from 'rxjs';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
+    animations:[
+      trigger("bounce", [transition(":increment", useAnimation(bounce, {
+        params:{timing:4, delay:2}
+      }))]),
+      trigger("shake", [transition(":increment", useAnimation(shake, {
+        params:{timing:2, delay:0}
+      }))]),
+      trigger("tada", [transition(":increment", useAnimation(tada, {
+        params:{timing:3, delay:5}
+      }))])
+    ],
     styleUrls: ['./app.component.css'],
     standalone: true
 })
 export class AppComponent {
   title = 'ngAnimations';
 
+  bounce = 0;
+  shake = 0;
+  tada = 0
   constructor() {
+  }
+
+  oneTime(boucleValue:boolean){
+    console.log(boucleValue)
+    this.shake++;
+    this.bounce++;
+    this.tada++;
+    setTimeout(() => {if(boucleValue){
+      this.oneTime(true)
+    }}, 8000);
+    
+    
+  }
+
+  rotate(){
+    var element = document.getElementById("turnId")
+    element?.classList.add("rotate-left")
+    setTimeout(() => {
+      element?.classList.remove("rotate-left")
+    }, 2000);
+
   }
 }
